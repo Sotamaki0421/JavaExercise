14239b02d27486a60b50241ebef90a6e14004a85
Sota Makino <sotamaki0421@gmail.com>
2021-12-15 02:35:55 +0900

    Get a substring using the substring () method.

---
+++ test/SubstringSample.java
@@ -0,0 +1,11 @@
+package test;
+
+public class SubstringSample {
+
+    public static void main(String[] args) {
+        String str = "あいうえおかきくけこ";
+        String sub = str.substring(4, 7);
+        System.out.println(sub);
+    }
+
+}
