5a72a0a8d8df98bf2064171c33d69efb50eb7b1e
Sota Makino <sotamaki0421@gmail.com>
2021-12-15 02:44:48 +0900

    Method of Math class.

---
+++ test/MathClassSample.java
@@ -0,0 +1,21 @@
+package test;
+
+public class MathClassSample {
+
+    public static void main(String[] args) {
+        double flr = Math.floor(12.99);
+        double rnd = Math.round(12.99);
+        double cl = Math.ceil(12.99);
+        double pow = Math.pow(9, 2);
+        double sqrt = Math.sqrt(144);
+        double rad = 10 * 10 * Math.PI;
+
+        System.out.println(flr);
+        System.out.println(rnd);
+        System.out.println(cl);
+        System.out.println(pow);
+        System.out.println(sqrt);
+        System.out.println(rad);
+    }
+
+}
