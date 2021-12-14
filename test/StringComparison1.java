e18874f966a655d475a9efd2f539adfcf0c4f885
Sota Makino <sotamaki0421@gmail.com>
2021-12-15 02:30:39 +0900

    Comparison of strings using ==.

--- test/StringComparison1.java
+++ test/StringComparison1.java
@@ -5,7 +5,6 @@ public class StringComparison1 {
     public static void main(String[] args) {
         String str1 = new String("abc");
         String str2 = new String("abc");
-
         if (str1 == str2) {
             System.out.println("一致しました");
         }else {
