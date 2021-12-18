a90fbca1e3551e40263013a209ec5a3e0cabdb3d
Sota Makino <sotamaki0421@gmail.com>
2021-12-18 17:38:20 +0900

    Overridden the superclass's introduce() and greeting() methods.

--- trainingOfInheritance/EmployeeSales.java
+++ trainingOfInheritance/EmployeeSales.java
@@ -7,17 +7,28 @@ public class EmployeeSales extends EmployeeBase {
     public EmployeeSales() {
     }
 
-    public EmployeeSales(int employeeId, String employeeName, int vaitality) {
-        super.setEmployeeId(employeeId);
-        super.setEmployeeName(employeeName);
-        super.setDivisionName("営業部");
-        super.setVitality(vaitality);
+    public EmployeeSales(int employeeId, String employeeName, int vitality) {
+        super(employeeId, employeeName, "営業部", vitality);
         this.appointment = "未定";
     }
 
     public void report() {
         super.setVitality(super.getVitality() - 10);
         System.out.println("今日は10件アポを取りました");
+    }
+
+    @Override
+    public void introduce() {
+        super.setVitality(super.getVitality() - 10);
+        System.out.println("私の名前は"+ super.getEmployeeName() +"です。");
+        System.out.println("私の所属部署は"+ super.getDivisionName() +"です。");
+        System.out.println("今日のアポイント先は"+ appointment +"です。");
+    }
+
+    @Override
+    public void greeting() {
+        super.setVitality(super.getVitality() - 10);
+        System.out.println(super.getDivisionName() + "の皆さん" + "おはようございます");
     }
 
     public String getAppointment() {
